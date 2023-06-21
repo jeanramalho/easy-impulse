318d61aff317f2fdceae1f1c5a64d4003725104c
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-21 14:24:20 -0300

    feat(page): Criando estrutura de estilo e páginas

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-import Header from '@/components/Header'
+import HomePage from '@/pages/HomePage'
 
 
 export default function Home() {
   return (
     <main>
-      <Header />
+      <HomePage />
     </main>
   )
 }
