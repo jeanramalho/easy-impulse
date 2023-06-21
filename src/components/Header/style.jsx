975b04327bdfdda4b8dbf0973d146474e6d6f711
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-21 15:56:03 -0300

    feat(Header): Ajustes no style

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
     color: #000;
     width: 100%;
-    height: 1.4rem;
+    height: 7vh;
     background: orange;
+    diplay: flex;
+    justify-content: center;
+    
 `
