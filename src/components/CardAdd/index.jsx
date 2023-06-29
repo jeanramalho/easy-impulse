d6b83ad3b4085abc939b32e23252a64a34494a04
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-29 14:53:40 -0300

    feat(CardAdd): Estiliza componente

--- src/components/CardAdd/index.jsx
+++ src/components/CardAdd/index.jsx
@@ -3,7 +3,8 @@ import { Container } from "./style";
 const CardAdd = () => {
     return (
         <Container>
-            
+            <h2>Adcionar Cliente</h2>
+            <p>+</p>
         </Container>
     )
 }
--- src/components/CardAdd/style.jsx
+++ src/components/CardAdd/style.jsx
@@ -5,4 +5,18 @@ export const Container = styled.ul`
     height: auto;
     padding: 5px;
     border-bottom: 2px #cecece solid;
+    color: #cecece;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+        font-size: 2rem;
+    }
+
+    h2 {
+        text-transform: uppercase;
+        font-size: 1.2rem;
+    }
 `
