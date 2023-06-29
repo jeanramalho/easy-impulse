49863340df950e6f891a6ded108e54fe96c3bf2f
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-29 15:45:50 -0300

    feat(CardClient): Implementa conteúdo no elemento

--- src/components/CardClient/index.jsx
+++ src/components/CardClient/index.jsx
@@ -3,7 +3,11 @@ import { Container } from "./style";
 const CardClient = () => {
     return (
         <Container>
-            
+            <div>
+                <h3>nome-do-cliente</h3>
+                <p>adofbaoignaogroa==</p>
+            </div>
+            <h2>&gt;</h2>
         </Container>
 
     )
