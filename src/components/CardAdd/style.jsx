ece90b7ff7dff522de38725f7ad95a2245e499c0
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-30 15:22:12 -0300

    feat(CardClient): Ajusta cor da font e borda

--- src/components/CardAdd/style.jsx
+++ src/components/CardAdd/style.jsx
@@ -4,8 +4,8 @@ export const Container = styled.ul`
     width: 100%;
     height: auto;
     padding: 5px;
-    border-bottom: 2px #cecece solid;
-    color: #cecece;
+    border-bottom: 2px #808080 solid;
+    color: #808080;
     display: flex;
     flex-direction: row;
     align-items: center;
