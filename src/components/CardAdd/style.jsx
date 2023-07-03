e49cd5a40a96e50f94f418627b035ebdbfd3d67f
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-03 11:53:11 -0300

    remove(Modal): Retira codigo de estilização do modal de onde não deveria ter sido colocado

--- src/components/CardAdd/style.jsx
+++ src/components/CardAdd/style.jsx
@@ -21,8 +21,4 @@ export const Container = styled.ul`
         font-size: 1.2rem;
     }
 
-    Modal {
-        background: red;
-    }
- 
 `
--- src/pages/HomePage/style.jsx
+++ src/pages/HomePage/style.jsx
@@ -4,26 +4,4 @@ export const Container = styled.div`
     background: #fff;
     width: 100vw;
     height: 100vh;
-
-    .modal-overlay {
-        background: #cecece;
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
-
-        display: flex;
-        aling-items: center;
-        justify-content: center;
-    }
-
-    .modal-content {
-        width: 90%;
-        max-width: 500px;
-        background: #fff;
-        padding: 10px;
-        border-radius: 5px;
-        
-    }
 `
