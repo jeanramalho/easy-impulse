6e38add7cf55b5859cd8afd25232d1813a9b990b
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-05 23:32:49 -0300

    feat(page.js): tenta realizar ajustes

--- src/app/page.js
+++ src/app/page.js
@@ -8,10 +8,11 @@ import { BrowserRouter } from 'react-router-dom'
 
 export default function Home() {
   return (
+    <BrowserRouter >
     <main id='Root'>
-      <BrowserRouter >
-        <Router />
-      </BrowserRouter>
+      
+        <Router />      
     </main>
+    </BrowserRouter>
   )
 }
