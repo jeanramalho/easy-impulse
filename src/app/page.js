ec368b97ebf280b9741fa79cf6fa595f055fa7c3
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-06 21:22:16 -0300

    rollback(HomePage): Fazendo rollback de react router dom

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,13 @@
 
 import Image from 'next/image'
 import HomePage from '@/pages/HomePage'
-import Router from "@/components/Router"
-import { BrowserRouter } from 'react-router-dom'
+
 
 
 export default function Home() {
   return (
     <main>
-      <BrowserRouter >
-        <Router />
-      </BrowserRouter>
+      <HomePage />
     </main>
   )
 }
