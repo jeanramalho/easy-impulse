a8529537d943ddaaedc4a9a7cca76af2d07774b9
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-06 21:37:01 -0300

    feat(RequestPage): Cria estutura de página de request)

--- src/pages/RequestPage/index.jsx
+++ src/pages/RequestPage/index.jsx
@@ -1,8 +1,12 @@
+import Header from "@/components/Header"
 
+import { Container } from "./style"
 
 const RequestPage = () => {
     return (
-
+        <Container>
+            <Header />
+        </Container>
     )
 }
 
