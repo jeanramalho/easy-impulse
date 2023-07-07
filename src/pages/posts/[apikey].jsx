933cef40ded564e076529cbedb1408abf5f27057
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-07 17:07:22 -0300

    feat([apikey]): Cria estrutura das rotas

--- src/pages/posts/[apikey].jsx
+++ src/pages/posts/[apikey].jsx
@@ -4,10 +4,11 @@ import { useRouter } from "next/router"
 
 const Apikey = () => {
 
-    
+    const router = useRouter()
+    const parametro = router.query.Apikey
 
     return (
-
+        <h1>{parametro}</h1>
     )
 }
 
