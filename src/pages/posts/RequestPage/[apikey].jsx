e856f9284de10ad28a959c27dc2b305a569ea355
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-07 17:19:37 -0300

    feat(RequestPage): Tentando implementar de forma inteligens a rota dinamica

--- src/pages/posts/RequestPage/[apikey].jsx
+++ src/pages/posts/RequestPage/[apikey].jsx
@@ -2,12 +2,18 @@
 
 import Header from "@/components/Header"
 
+import { useRouter } from "next/router"
+
 import { Container } from "./style"
 
 const RequestPage = () => {
+    const router = useRouter()
+    const parametro = router.query.apikey
+
     return (
         <Container>
             <Header />
+            <h1>A apikey é: {parametro}</h1>
         </Container>
     )
 }
