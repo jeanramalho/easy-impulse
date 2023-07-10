fd5d33ed40111a308c745b08eb51a9404f46d937
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-10 18:05:11 -0300

    feat(ListClients): Implementando map em objeto

--- src/components/ListClients/index.jsx
+++ src/components/ListClients/index.jsx
@@ -3,6 +3,13 @@ import { Container } from "./style"
 import CardAdd from "../CardAdd"
 import CardClient from "../CardClient"
 
+const obj = [
+    {"apikey": "tokstoken", "secretKey":"sdfsdfsd"},
+    {"apikey": "paodeacucar", "secretKey":"sdfsdfsd"},
+    {"apikey": "clubeextra", "secretKey":"sdfsdfsd"},
+    
+]
+
 
 const ListClients = () => {
     return (
