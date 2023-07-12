ee203bf68914bbaf9c27d1dfb27ff58c0170cd7f
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-12 12:22:42 -0300

    feat(ListClients): Implementa código para retornar um componente para cada cliente salvo no local storage

--- src/components/ListClients/index.jsx
+++ src/components/ListClients/index.jsx
@@ -1,17 +1,27 @@
 import { Container } from "./style"
+import { useEffect } from "react"
 import CardAdd from "../CardAdd"
 import CardClient from "../CardClient"
 
 
 
 const ListClients = () => {
+   
+        const clients = localStorage.getItem('clientData')
+        const cardClients = JSON.parse(clients)
+    
+        console.log(cardClients)
+    
+    
+    
     return (
         <Container>
             <CardAdd />
-            {/* {obj.map((objeto) => {
-                
-               return(<CardClient apikey={objeto.apikey} secretkey={objeto.secretkey}/>) 
-            })} */}
+           {
+            cardClients.map((cliente) => {
+                return ( <CardClient key={cliente.apikey} apikey={cliente.apikey} secretkey={cliente.secretKey}/> )
+            })
+           }
         </Container>
     )
 }
