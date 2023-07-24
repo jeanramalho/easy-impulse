b877fb48d9c0406578b1f8d056232f2a7ebc6415
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-24 11:35:59 -0300

    feat(SwitchRequest): Criando encapsulamento de container

--- src/components/SwitchRequest/index.jsx
+++ src/components/SwitchRequest/index.jsx
@@ -1,7 +1,11 @@
 import Styles from './SwitchRequest.module.scss'
 
 const SwitchRequest = () => {
-
+    return (
+        <div className="container">
+            
+        </div>
+    )
 }
 
 export default SwitchRequest
