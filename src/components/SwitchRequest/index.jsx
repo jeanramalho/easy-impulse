32eb9674643b6bdfab34f04156c2a24fc0633fdf
jeanramalho <jeanramalho.dev@gmail.com>
2023-07-24 11:46:21 -0300

    feat(SwitchRequest): Cria estrutura de selecionar api

--- src/components/SwitchRequest/index.jsx
+++ src/components/SwitchRequest/index.jsx
@@ -3,7 +3,15 @@ import Styles from './SwitchRequest.module.scss'
 const SwitchRequest = () => {
     return (
         <div className="container">
-            
+            <form>
+                <select name="selectedApi">
+                    <option value=""> Escolha uma API para request </option>
+                    <option value="xbox">Xbox Series</option>
+                    <option value="ps5">PS5</option>
+                    <option value="nsw">Nintendo Switch</option>
+                    <option value="pcg">PC Gaming</option>
+                </select>
+            </form>
         </div>
     )
 }
